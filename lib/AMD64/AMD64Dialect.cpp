4069454d01164130c781ee4ded9fc212bc4cd346
Jim M. R. Teichgräber <jimmr.teichgraeber@tum.de>
2023-05-01 18:03:45 +0200

    start a MemoryOperandAttribute, which won't be used now, use a custom op instead

--- lib/AMD64/AMD64Dialect.cpp
+++ lib/AMD64/AMD64Dialect.cpp
@@ -14,6 +14,37 @@ using namespace amd64;
 
 #include "AMD64/AMD64OpsDialect.cpp.inc"
 
+namespace amd64{
+struct MemoryOperandAttr : public mlir::Attribute::AttrBase<MemoryOperandAttr, mlir::Attribute, mlir::AttributeStorage> { // TODO needs an actual storage class, with displacement and scale ints
+    // inherit necessary constructors
+    using Base::Base;
+
+    /// vaguely inspired by the VerCapExtAttr from SPIRVAttributes.h/cpp, which fulfills a similar purpose (structured info about an op)
+
+    /// Create an instance from concrete values, these get converted to attributes which are passed to the second get() function, which passes them to the Base
+    static MemoryOperandAttr get(int32_t displacement, uint8_t scale, mlir::MLIRContext* ctx){
+        mlir::Builder builder(ctx);
+        auto displacementAttr = builder.getI32IntegerAttr(displacement);
+        auto scaleAttr = builder.getI8IntegerAttr(scale);
+        return get(displacementAttr, scaleAttr, ctx);
+    }
+
+    static MemoryOperandAttr get(mlir::IntegerAttr displacement, mlir::IntegerAttr scale, mlir::MLIRContext* ctx){
+        return Base::get(ctx, displacement, scale);
+    }
+
+    /// Getters for the values
+    int32_t getDisplacement(){
+        return getImpl()->displacement.getInt();
+    }
+
+    uint8_t getScale(){
+        return getImpl()->scale.getInt();
+    }
+
+};
+} // end namespace amd64
+
 void AMD64Dialect::initialize() {
   addOperations<
 #define GET_OP_LIST
