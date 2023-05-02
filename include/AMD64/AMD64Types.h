832925fb376956554983cc26af03360805b964cf
Jim M. R. Teichgräber <jimmr.teichgraeber@tum.de>
2023-05-02 16:01:18 +0200

    the RegConstraint thing was completely wrong, what was I thinking

--- include/AMD64/AMD64Types.h
+++ include/AMD64/AMD64Types.h
@@ -7,31 +7,6 @@
 
 using FeMnem = uint64_t;
 
-// TODO maybe this is premature optimization..., but I think it certainly makes the code more readable
-struct RegConstraint{
-    // lowest 8 bits represent the first constraint, next 8 bits represent the second constraint, etc.
-    uint32_t constraints;
-
-#define NO_CONSTRAINT static_cast<FeReg>(0)
-    RegConstraint(FeReg constraint1 = NO_CONSTRAINT, FeReg constraint2 = NO_CONSTRAINT, FeReg constraint3 = NO_CONSTRAINT, FeReg constraint4 = NO_CONSTRAINT) : 
-        constraints(transform(constraint1) | transform(constraint2) << 8 | transform(constraint3) << 16 | transform(constraint4) << 24) {}
-#undef NO_CONSTRAINT
-
-    template<uint8_t N>
-    requires (N < 4)
-    bool constrainsOpN(FeReg reg){
-        // TODO is this right?
-		constexpr auto shiftLeftBy = N*8;
-        return (constraints & (0xFFFFFFFF << shiftLeftBy)) == static_cast<uint32_t>(transform(reg) << shiftLeftBy);
-    }
-
-private:
-    uint8_t transform(FeReg r){
-        return r >> 8;
-    }
-
-};
-
 #define GET_TYPEDEF_CLASSES
 #include "AMD64/AMD64OpsTypes.h.inc"
 
