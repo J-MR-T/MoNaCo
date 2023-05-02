112992fffa48b339f8d5d4a967856540f43a9344
Jim M. R. Teichgräber <jimmr.teichgraeber@tum.de>
2023-05-02 15:44:49 +0200

    add `RegConstraint` class which might be used to model register constraints correctly, not decided yet

--- include/AMD64/AMD64Types.h
+++ include/AMD64/AMD64Types.h
@@ -3,8 +3,33 @@
 
 #include "mlir/IR/BuiltinTypes.h"
 
+#include "fadec-enc.h"
 
 using FeMnem = uint64_t;
+
+// TODO maybe this is premature optimization..., but I think it certainly makes the code more readable
+struct RegConstraint{
+    // lowest 8 bits represent the first constraint, next 8 bits represent the second constraint, etc.
+    uint32_t constraints;
+
+#define NO_CONSTRAINT static_cast<FeReg>(0)
+    RegConstraint(FeReg constraint1 = NO_CONSTRAINT, FeReg constraint2 = NO_CONSTRAINT, FeReg constraint3 = NO_CONSTRAINT, FeReg constraint4 = NO_CONSTRAINT) : 
+        constraints(transform(constraint1) | transform(constraint2) << 8 | transform(constraint3) << 16 | transform(constraint4) << 24) {}
+#undef NO_CONSTRAINT
+
+    template<uint8_t N>
+    requires (N < 4)
+    bool constrainsOpN(FeReg reg){
+        // TODO is this right?
+        return constraints == static_cast<uint32_t>(transform(reg) << (N*8));
+    }
+
+private:
+    uint8_t transform(FeReg r){
+        return r >> 8;
+    }
+
+};
 
 #define GET_TYPEDEF_CLASSES
 #include "AMD64/AMD64OpsTypes.h.inc"
