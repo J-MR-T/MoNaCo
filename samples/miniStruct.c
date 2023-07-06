df330dee453738009e1d3a77c66f24f497965b09
Jim M. R. Teichgräber <jimmr.teichgraeber@tum.de>
2023-07-06 19:24:24 +0200

    add 'miniStruct' test

---
+++ samples/miniStruct.c
@@ -0,0 +1,21 @@
+
+typedef struct {
+    int a;
+    long long b;
+    struct {
+        int c;
+        int d;
+    } inner;
+} a;
+
+int main(int argc, char *argv[])
+{
+    a as[2];
+    as[0].a = 10;
+    as[1].b = 20;
+
+    as[0].inner.c = 30;
+    as[1].inner.d = 40;
+
+    return 0;
+}
