51ed431b7277f6026888725dbd22d0a22219a6ae
Jim M. R. Teichgräber <jimmr.teichgraeber@tum.de>
2023-07-20 17:15:13 +0200

    fix and add to global int C test

--- tests/intGlobal.c
+++ tests/intGlobal.c
@@ -1,6 +1,8 @@
 // RUN: %RunC 'main' | FileCheck %s
+// RUN: %RunC 'main' -pasm 2>&1 | FileCheck --ignore-case --check-prefix ASM %s
 
-// CHECK: 56
+// ASM-NOT: warning: global gloooob is not at the expected address
+// ASM: warning: global printf is not at the expected address{{.*}}could be external
 
 int gloooob = 5;
 
@@ -9,5 +11,6 @@ int main(int argc, char *argv[])
     printf("%d", gloooob);
     gloooob = 6;
     printf("%d\n", gloooob);
+// CHECK: 56
     return 0;
 }
