3a0d09a471d60f370a29799683d634de99a9176b
Jim M. R. Teichgräber <jimmr.teichgraeber@tum.de>
2023-08-05 17:15:09 +0200

    expand addrofFunc.c

--- tests/addrofFunc.c
+++ tests/addrofFunc.c
@@ -1,9 +1,11 @@
-// RUN: %RunC main | FileCheck %s
+// RUN: %RunC main | FileCheck --match-full-lines %s
 #include <stdio.h>
 
 void test(void){
     puts("Hello!");
 }
+
+void test2(void);
 
 int main(int argc, char *argv[]) {
     void (*testFunc)(void) = &test;
@@ -11,5 +13,17 @@ int main(int argc, char *argv[]) {
     printf("testFunc: %p\n", testFunc);
     // CHECK-NEXT: Hello!
     testFunc();
+    testFunc = &test2;
+    // CHECK-NEXT: Hello2!
+    testFunc();
+
+    // now call putchar indirectly
+    int (*putcharFunc)(int) = &putchar;
+    putcharFunc('a');
+    // CHECK-NEXT: a
     return 0;
+}
+
+void test2(void){
+    puts("Hello2!");
 }
