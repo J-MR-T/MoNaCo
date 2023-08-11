712734932168f4c5edac89edc295df515b7a56f0
Jim M. R. Teichgräber <jimmr.teichgraeber@tum.de>
2023-08-11 12:27:44 +0200

    better lit configuration for different monaco executables

--- lit.cfg.py
+++ lit.cfg.py
@@ -1,8 +1,6 @@
 import lit.formats
 import os
 
-# TODO find an option to automatically set -j1 per default for lit
-
 config.name = "MoNaCo: MLIR to Native Compiler"
 config.test_format = lit.formats.ShTest(True)
 
@@ -13,11 +11,18 @@ config.excludes = ['samples']
 config.test_source_root = os.path.dirname(__file__)
 config.test_exec_root = os.path.join(config.test_source_root, 'tests')
 
-config.substitutions.append(("%monaco", os.path.join(config.test_source_root, "monaco")))
+monacoExe = os.getenv("MONACO_EXE")
+if monacoExe is None:
+    monacoExe = os.path.join(config.test_source_root, "monaco")
+
+monacoExe = os.path.abspath(monacoExe)
+
+config.substitutions.append(("%monaco", monacoExe))
 config.substitutions.append(("%FileCheckAsm", r"%monaco -p asm %s | FileCheck"))
 # doesn't work anymore with the mmapping stuff, but we don't need it, just use the jit execution engine
 # config.substitutions.append(("%FileCheckExecReturnStatus", r"""outfileName=$(basename -z %basename_t .mlir); %monaco %s $outfileName && objcopy --input-target=binary --output-target=elf64-x86-64 --rename-section '.data=.text' $outfileName %t_2 && gcc -c -x c <(echo "int main(int argc, char** argv){ return _binary_"$outfileName"_start(argc, argv); } ") -o %t_3 && gcc %t_2 %t_3 -o %t_4 && %t_4; echo $? | FileCheck"""))
 config.substitutions.append(("%FileCheckExecReturnStatus", r"""%monaco --jit "main" %s; echo $? | FileCheck"""))
+# would be nice to be able to do this on all optimization levels
 config.substitutions.append(("%RunC", r"""clang -S -emit-llvm %s -o - 2>/dev/null | mlir-translate --import-llvm 2>/dev/null | %monaco /dev/stdin --jit """))
 
 config.recursiveExpansionLimit = 3
